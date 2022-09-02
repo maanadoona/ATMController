666d7ef6d8ec65b2e963c96c9d8ff184b186bf18
Brian Song <maanadoona@gmail.com>
2022-09-03 00:17:25 +0900

    atmController Class Init
    1. Class Initialization
    2. function Initialization

--- atmController.py
+++ atmController.py
@@ -1,4 +1,22 @@
 
 
 class atmController():
-    pass
+    def __init__(self):
+        self.cardStatus = 0
+        self.pinNumber = "0000"
+        self.account = "1234567"
+        self.balance = 0
+        self.deposit = 0
+        self.withdraw = 0
+
+    def InsertCard(self, card):
+        pass
+
+    def PINnumber(self, pin):
+        pass
+
+    def SelectAccount(self, account):
+        pass
+
+    def ShowAccountInfo(self):
+        return self.balance, self.deposit, self.withdraw
