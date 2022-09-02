90516fa56351f15b882f4393b0a42d7333e64ada
Brian Song <maanadoona@gmail.com>
2022-09-03 00:36:03 +0900

    Function Implement
    1) Define Enums
    2) InsertCard Function implement
    3) PINnumber Function implement

--- atmController.py
+++ atmController.py
@@ -1,19 +1,43 @@
+from enum import Enum
 
 
-class atmController():
+class CardStatus(Enum):
+    NotInserted = 0
+    Inserted = 1
+
+class PinStatus(Enum):
+    Incorrect = 0
+    Correct = 1
+
+class AccountStatus(Enum):
+    NotSelected = 0
+    Selected = 1
+
+class AtmController():
     def __init__(self):
-        self.cardStatus = 0
+        self.cardStatus = CardStatus.NotInserted
         self.pinNumber = "0000"
+        self.pinStatus = PinStatus.Incorrect
         self.account = "1234567"
+        self.accountStatus = AccountStatus.NotSelected
         self.balance = 0
         self.deposit = 0
         self.withdraw = 0
 
     def InsertCard(self, card):
+        self.cardStatus = card
         pass
 
     def PINnumber(self, pin):
-        pass
+        # PIN Number Rule Check
+        if len(pin) != 4:   # 1) size : 4
+            self.pinStatus = PinStatus.Incorrect
+        else:               # 2) 0~9
+            if pin.isdigit() is False:
+                self.pinStatus = PinStatus.Incorrect
+            else:
+                self.pinStatus = PinStatus.Correct
+                self.pinNumber = pin
 
     def SelectAccount(self, account):
         pass
